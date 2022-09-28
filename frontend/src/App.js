4fe6220bfc6a352f8ddf52940db0e14ea4e684cb
sanju <sanjusabu066@gmail.com>
2022-09-28 13:27:56 +0530

    reload and everything is changing error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useCallback, useEffect } from "react";
+import Tasks from "./components/Tasks/Tasks";
+import { AuthContext } from "../src/context/authcontext";
+
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const [userId, setuserId] = useState("");
+
+  const login = useCallback((uid) => {
+    localStorage.setItem("userid", uid);
+    setIsLoggedIn(true);
+    setuserId(localStorage.getItem("userid"));
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+    setuserId(null);
+  }, []);
+
+  useEffect(() => {
+    if (localStorage.hasOwnProperty("userid")) {
+      setuserId(localStorage.getItem("userid"));
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  let routes;
+  if (!isLoggedIn) {
+    routes = (
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+          {/* <Route path="/register" element={<Register />}></Route> */}
+          <Route path="/" element={<Register />}></Route>
+        </Routes>
+      </Router>
+    );
+  } else {
+    routes = (
+      <Router>
+        <Routes>
+          <Route path="/tasks" element={<Tasks />}></Route>
+        </Routes>
+      </Router>
+    );
+  }
   return (
-    <div className="App">
-     <Router>
-      <Routes>
-     <Route path="/login" element={<Login />}></Route>
-     <Route path="/register" element={<Register />}></Route>
-     <Route path="*" element={<Register />}></Route>
-    </Routes>
-     </Router>
-    </div>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        userId: userId,
+        login: login,
+        logout: logout,
+      }}
+    >
+      <main>{routes}</main>
+    </AuthContext.Provider>
   );
 }
-
 export default App;
